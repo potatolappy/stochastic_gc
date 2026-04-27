3313d6d4d20f21a07000dad792ba617860bf182c
Haris Rasyidi <138958088+potatolappy@users.noreply.github.com>
2026-04-27 22:04:11 +0700

    Add new stock symbols to the list in app.py

--- app.py
+++ app.py
@@ -327,6 +327,7 @@ IDX_TICKERS = list(dict.fromkeys([
     "BNBR.JK","DEWA.JK","BULL.JK","PPRE.JK","SMAA.JK","KIJA.JK",
     "VKTR.JK","ELSA.JK","OILS.JK","BULL.JK","WIIM.JK","BDMN.JK","ISAT.JK","TPIA.JK","TPIA.JK",
     "DKFT.JK","ENRG.JK","BIPI.JK","WIFI.JK","INET.JK","ESSA.JK","MBMA.JK", "AMMN.JK","ANTM.JK","MDKA.JK","EMTK.JK","BRMS.JK",   
+    "SMGR.JK","NCKL.JK","TRIM.JK",
 ]))
 
 # ── Data fetch ────────────────────────────────────────────────────────────────
